db7231b094ca466fcaf51acfd100f9f94be59d83
John Millikin <jmillikin@gmail.com>
2011-11-02 20:28:15 -0700

    Re-enable testing ``listDirectory`` on Windows.

--- tests/FilesystemTests/Windows.hs
+++ tests/FilesystemTests/Windows.hs
@@ -10,35 +10,25 @@ module FilesystemTests.Windows
 	( test_Windows
 	) where
 
-import           Control.Monad.IO.Class (liftIO)
-import qualified Data.Text
-import           System.IO.Temp (withSystemTempDirectory)
 import           Test.Chell
 
 import           Filesystem
 import           Filesystem.Path.CurrentOS
 
 test_Windows :: Suite
-test_Windows = suite "windows" []
+test_Windows = suite "windows"
+	[ test_ListDirectory
+	]
 
-test_UnicodeWindows :: Suite
-test_UnicodeWindows = assertions "windows" $ do
-	let text = "\12354\946\1076\119070.txt"
-	(tempDir, paths, contents) <- liftIO $ withSystemTempDirectory "tests." $ \dir -> do
-		let dirPath = decodeString dir
-		let filePath = dirPath </> fromText text
-		writeTextFile filePath "contents"
-		
-		paths <- listDirectory dirPath
-		contents <- readTextFile filePath
-		return (dir, paths, contents)
+test_ListDirectory :: Suite
+test_ListDirectory = assertionsWithTemp "listDirectory" $ \dir -> do
+	let paths =
+		[ dir </> decode "test.txt"
+		, dir </> decode "\12354\946\1076\119070.txt"
+		, dir </> decode "\xA1\xA2\xA3.txt"
+		]
 	
-	$assert (not (Prelude.null paths))
-	$expect $ equal (length paths) 1
-	let path = paths !! 0
+	liftIO $ forM_ paths (\path -> writeTextFile path "")
 	
-	let textPath = Data.Text.concat
-		[ Data.Text.pack tempDir, "\\", text]
-	$expect $ equal (toText path) (Right textPath)
-	
-	$expect $ equal contents "contents"
+	names <- liftIO $ Filesystem.listDirectory dir
+	$expect $ sameItems paths names
