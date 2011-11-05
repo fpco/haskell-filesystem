7d80150a6394d4eb9cbe8936315ad64bb60a459a
John Millikin <jmillikin@gmail.com>
2011-11-05 10:20:32 -0700

    Benchmarks: fix UTF8 sample, run each test 100 times for better resolution.

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -28,7 +28,7 @@ ascii :: String
 ascii = "/abcdefg/abcdefg/abcdefg.abc.def.ghi"
 
 utf8 :: String
-utf8 = "/\xC3\x88\xC3\x89\xC3\x8a\xC3\x8b\xC3\x8C/\xC3\x88\xC3\x89\xC3\x8A\xC3\x8B\xC3\x8C/\xC3\x88\xC3\x89\xC3\x8A\xC3\x8B\xC3\x8C.\xC3\x88\xC3\x89.\xC3\x8A\xC3\x8B.\x8C"
+utf8 = "/\xC3\x88\xC3\x89\xC3\x8a\xC3\x8b\xC3\x8C/\xC3\x88\xC3\x89\xC3\x8A\xC3\x8B\xC3\x8C/\xC3\x88\xC3\x89\xC3\x8A\xC3\x8B\xC3\x8C.\xC3\x88\xC3\x89.\xC3\x8A\xC3\x8B.\xC3\x8C"
 
 iso8859 :: String
 iso8859 = "/\xC8\xC9\xCA\xCB\xCC/\xC8\xC9\xCA\xCB\xCC/\xC8\xC9\xCA\xCB\xCC.\xC8\xC9.\xCA\xCB.\xCC"
@@ -69,33 +69,36 @@ posix_iso8859 :: FilePath
 posix_iso8859 = decode posix bytes_iso8859
 {-# NOINLINE posix_iso8859 #-}
 
+nfMap :: NFData b => Int -> (a -> b) -> a -> Pure
+nfMap n f x = let xs = replicate n x in xs `seq` nf (map f) xs
+
 benchmarks :: [Benchmark]
 benchmarks =
 	[ bgroup "posix"
 	  [ bgroup "to-text"
-	    [ bench "ascii" (nf (toText posix) posix_ascii)
-	    , bench "utf8" (nf (toText posix) posix_utf8)
-	    , bench "iso8859" (nf (toText posix) posix_iso8859)
+	    [ bench "ascii" (nfMap 100 (toText posix) posix_ascii)
+	    , bench "utf8" (nfMap 100 (toText posix) posix_utf8)
+	    , bench "iso8859" (nfMap 100 (toText posix) posix_iso8859)
 	    ]
 	  , bgroup "to-bytes"
-	    [ bench "ascii" (nf (encode posix) posix_ascii)
-	    , bench "utf8" (nf (encode posix) posix_utf8)
-	    , bench "iso8859" (nf (encode posix) posix_iso8859)
+	    [ bench "ascii" (nfMap 100 (encode posix) posix_ascii)
+	    , bench "utf8" (nfMap 100 (encode posix) posix_utf8)
+	    , bench "iso8859" (nfMap 100 (encode posix) posix_iso8859)
 	    ]
 	  , bgroup "to-ghc"
-	    [ bench "ascii" (nf (encodeString posix_ghc702) posix_ascii)
-	    , bench "utf8" (nf (encodeString posix_ghc702) posix_utf8)
-	    , bench "iso8859" (nf (encodeString posix_ghc702) posix_iso8859)
+	    [ bench "ascii" (nfMap 100 (encodeString posix_ghc702) posix_ascii)
+	    , bench "utf8" (nfMap 100 (encodeString posix_ghc702) posix_utf8)
+	    , bench "iso8859" (nfMap 100 (encodeString posix_ghc702) posix_iso8859)
 	    ]
 	  , bgroup "from-text"
-	    [ bench "ascii" (nf (fromText posix) text_ascii)
-	    , bench "utf8" (nf (fromText posix) text_utf8)
-	    , bench "iso8859" (nf (fromText posix) text_iso8859)
+	    [ bench "ascii" (nfMap 100 (fromText posix) text_ascii)
+	    , bench "utf8" (nfMap 100 (fromText posix) text_utf8)
+	    , bench "iso8859" (nfMap 100 (fromText posix) text_iso8859)
 	    ]
 	  , bgroup "from-bytes"
-	    [ bench "ascii" (nf (decode posix) bytes_ascii)
-	    , bench "utf8" (nf (decode posix) bytes_utf8)
-	    , bench "iso8859" (nf (decode posix) bytes_iso8859)
+	    [ bench "ascii" (nfMap 100 (decode posix) bytes_ascii)
+	    , bench "utf8" (nfMap 100 (decode posix) bytes_utf8)
+	    , bench "iso8859" (nfMap 100 (decode posix) bytes_iso8859)
 	    ]
 	  ]
 	]
