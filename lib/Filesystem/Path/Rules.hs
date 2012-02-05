67233c8f74e5f187806e8705cb3a408a1583c2ab
John Millikin <jmillikin@gmail.com>
2012-02-05 12:59:02 -0800

    Fix "Since" annotations on posix_ghc702, darwin, and darwin_ghc702.
    Having "Since 0.3.x" in the 0.4 is confusing, because people assume that 0.4
    has all the features of the latest 0.3.

--- lib/Filesystem/Path/Rules.hs
+++ lib/Filesystem/Path/Rules.hs
@@ -77,7 +77,7 @@ posix = Rules
 -- This is a variant of 'posix' for use with GHC 7.2 or later, which tries to
 -- decode file paths in its IO computations.
 --
--- Since: 0.3.3
+-- Since: 0.4.2
 posix_ghc702 :: Rules B.ByteString
 posix_ghc702 = posix
 	{ rulesName = T.pack "POSIX (GHC 7.2)"
@@ -187,7 +187,7 @@ posixSplitSearch = map (posixFromBytes . normSearch) . B.split 0x3A where
 -- This is almost identical to 'posix', but with a native path type of 'T.Text'
 -- rather than 'B.ByteString'.
 --
--- Since: 0.3.4
+-- Since: 0.4.3
 darwin :: Rules T.Text
 darwin = Rules
 	{ rulesName = T.pack "Darwin"
@@ -206,7 +206,7 @@ darwin = Rules
 -- This is a variant of 'darwin' for use with GHC 7.2 or later, which tries to
 -- decode file paths in its IO computations.
 --
--- Since: 0.3.4
+-- Since: 0.4.3
 darwin_ghc702 :: Rules T.Text
 darwin_ghc702 = darwin
 	{ rulesName = T.pack "Darwin (GHC 7.2)"
