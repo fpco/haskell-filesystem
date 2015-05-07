d161a0afb0929832416a14539e740a212aa306f2
Tim Dysinger <tim@dysinger.net>
2015-05-06 18:23:53 -1000

    add TODO markers

--- system-filepath/lib/Filesystem/Path.hs
+++ system-filepath/lib/Filesystem/Path.hs
@@ -132,77 +132,77 @@ posix_ghc704 :: Platform B.ByteString
 posix_ghc704 = Platform FPI.posix_ghc704
 
 toText :: FilePath -> Either T.Text T.Text
-toText = FPI.toText . toIFP
+toText = FPI.toText . toIFP -- TODO impl custom replacement
 
 fromText :: T.Text -> FilePath
-fromText = fromIFP . FPI.fromText
+fromText = fromIFP . FPI.fromText -- TODO impl custom replacement
 
 valid :: FilePath -> Bool
 valid = SF.isValid . unFilePath
 
 splitSearchPath :: PLATFORM_PATH_FORMAT -> [FilePath]
-splitSearchPath = map fromIFP . FPI.splitSearchPath
+splitSearchPath = map fromIFP . FPI.splitSearchPath -- TODO impl custom replacement
 
 splitSearchPathString :: String -> [FilePath]
-splitSearchPathString = map fromIFP . FPI.splitSearchPathString
+splitSearchPathString = map fromIFP . FPI.splitSearchPathString -- TODO impl custom replacement
 
 encode :: FilePath -> PLATFORM_PATH_FORMAT
-encode = FPI.encode . toIFP
+encode = FPI.encode . toIFP -- TODO impl custom replacement
 
 decode :: PLATFORM_PATH_FORMAT -> FilePath
-decode = fromIFP . FPI.decode
+decode = fromIFP . FPI.decode -- TODO impl custom replacement
 
 encodeString :: FilePath -> String
-encodeString = FPI.encodeString . toIFP
+encodeString = FPI.encodeString . toIFP -- TODO impl custom replacement
 
 decodeString :: String -> FilePath
-decodeString = fromIFP . FPI.decodeString
+decodeString = fromIFP . FPI.decodeString -- TODO impl custom replacement
 
 validOn :: forall a.
            Platform a -> FilePath -> Bool
-validOn p = FPI.validOn (unPlatform p) . toIFP
+validOn p = FPI.validOn (unPlatform p) . toIFP -- TODO impl custom replacement
 -- validOn _p | _ = Windows.isValid . unFilePath
 -- validOn _p | _ = Posix.isValid . unFilePath
 
 splitSearchPathOn :: forall a.
                      Platform a -> a -> [FilePath]
-splitSearchPathOn p = map fromIFP . FPI.splitSearchPathOn (unPlatform p)
+splitSearchPathOn p = map fromIFP . FPI.splitSearchPathOn (unPlatform p) -- TODO impl custom replacement
 
 splitSearchPathStringOn :: forall a.
                            Platform a -> String -> [FilePath]
-splitSearchPathStringOn p = map fromIFP . FPI.splitSearchPathStringOn (unPlatform p)
+splitSearchPathStringOn p = map fromIFP . FPI.splitSearchPathStringOn (unPlatform p) -- TODO impl custom replacement
 
 toTextOn :: forall a.
             Platform a -> FilePath -> Either T.Text T.Text
-toTextOn p = FPI.toTextOn (unPlatform p) . toIFP
+toTextOn p = FPI.toTextOn (unPlatform p) . toIFP -- TODO impl custom replacement
 
 fromTextOn :: forall a.
               Platform a -> T.Text -> FilePath
-fromTextOn p = fromIFP . FPI.fromTextOn (unPlatform p)
+fromTextOn p = fromIFP . FPI.fromTextOn (unPlatform p) -- TODO impl custom replacement
 
 encodeOn :: forall a.
             Platform a -> FilePath -> a
-encodeOn p = FPI.encodeOn (unPlatform p) . toIFP
+encodeOn p = FPI.encodeOn (unPlatform p) . toIFP -- TODO impl custom replacement
 
 decodeOn :: forall a. Platform a -> a -> FilePath
-decodeOn p = fromIFP . FPI.decodeOn (unPlatform p)
+decodeOn p = fromIFP . FPI.decodeOn (unPlatform p) -- TODO impl custom replacement
 
 encodeStringOn :: forall a.
                   Platform a -> FilePath -> String
-encodeStringOn p = FPI.encodeStringOn (unPlatform p) . toIFP
+encodeStringOn p = FPI.encodeStringOn (unPlatform p) . toIFP -- TODO impl custom replacement
 
 decodeStringOn :: forall a.
                   Platform a -> String -> FilePath
-decodeStringOn p = fromIFP . FPI.decodeStringOn (unPlatform p)
+decodeStringOn p = fromIFP . FPI.decodeStringOn (unPlatform p) -- TODO impl custom replacement
 
 empty :: FilePath
 empty = fromString ""
 
 null :: FilePath -> Bool
-null = FPI.null . toIFP
+null = FPI.null . toIFP -- TODO impl custom replacement
 
 root :: FilePath -> FilePath
-root = fromIFP . FPI.root . toIFP
+root = fromIFP . FPI.root . toIFP -- TODO impl custom replacement
 
 directory :: FilePath -> FilePath
 directory =
@@ -211,7 +211,7 @@ directory =
   unFilePath
 
 parent :: FilePath -> FilePath
-parent = fromIFP . FPI.parent . toIFP
+parent = fromIFP . FPI.parent . toIFP -- TODO impl custom replacement
 
 filename :: FilePath -> FilePath
 filename = fromString . SF.takeFileName . unFilePath
@@ -235,6 +235,11 @@ relative = FPI.relative . toIFP
 append :: FilePath -> FilePath -> FilePath
 append x y = fromIFP (FPI.append (toIFP x) (toIFP y))
   -- fromString (SF.combine (unFilePath x) (unFilePath y))
+  {-
+     FIXME:
+     system-filepath: "a" </> "" == "a/"
+     filepath: "a" </> "" == "a"
+  -}
 
 (</>) :: FilePath -> FilePath -> FilePath
 (</>) = append
@@ -253,7 +258,12 @@ collapse = fromIFP . FPI.collapse . toIFP -- TODO impl custom replacement
 
 splitDirectories :: FilePath -> [FilePath]
 splitDirectories = map fromIFP . FPI.splitDirectories . toIFP
--- splitDirectories = map fromString . SF.splitDirectories . unFilePath
+  -- map fromString . SF.splitDirectories . unFilePath
+  {-
+     FIXME:
+     system-filepath: splitDirectories "/ab/cd.txt" == ["/","ab/","cd.txt"]
+     filepath: splitDirectories "/ab/cd.txt" == ["/","ab","cd.txt"]
+  -}
 
 extension :: FilePath -> Maybe T.Text
 extension p =
